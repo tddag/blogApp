c61fa12a17aa04944f8dd5d225c5cd27045ada81
Tam Dang <danhtam0509@gmail.com>
2023-04-30 00:13:29 -0400

    initial setup for nextjs, tailwind, and layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+   <>
+    <Head>
+      <title>TD Blog</title>
+    </Head>
+    <Component {...pageProps} />
+   </>
+  )
 }
