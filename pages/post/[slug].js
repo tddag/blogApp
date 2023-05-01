2b8cfafb87bfcdf0d870841f34c8f58f789e1c91
tddag <danhtam0509@gmail.com>
2023-05-01 09:07:16 -0400

    finished categories

---
+++ pages/post/[slug].js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { getPosts, getPostDetails} from '../../services';
+
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } from '../../components'
+
+const PostDetails = () => {
+  return (
+    <div>
+        Post Details
+    </div>
+  )
+}
+
+export default PostDetails
