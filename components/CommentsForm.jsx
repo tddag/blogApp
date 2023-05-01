8e24085b2d8b310f063626a611c1dcf09b84871c
tddag <danhtam0509@gmail.com>
2023-05-01 16:52:55 -0400

    finished CommentsForm, query to write data to GraphCMS, backend api

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,6 +1,8 @@
 import React, { useRef, useState, useEffect} from 'react'
 
-const CommentsForm = () => {
+import { submitComment } from '../services';
+
+const CommentsForm = ({ slug }) => {
   const [error, setError] = useState(false)
   const [localStorage, setLocalStorage] = useState(null)
   const [showSuccessMessage, setShowSuccessMessage] = useState(false)
@@ -10,13 +12,49 @@ const CommentsForm = () => {
   const emailEl = useRef();
   const storeDataEl = useRef();
 
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem('name')
+    emailEl.current.value = window.localStorage.getItem('email')
+  }, [])
+
   const handleCommentSubmission = () => {
+    setError(false)
+
+    const { value: comment } = commentEl.current
+    const { value: name } = nameEl.current
+    const { value: email } = emailEl.current
+    const { checked: storeData } = storeDataEl.current
+
+    if(!comment || !name || !email) {
+      setError(true)
+      return;
+    }
+
+    const commentObj = {
+      name, email, comment, slug
+    }
+
+    if (storeData) {
+      window.localStorage.setItem('name', name)
+      window.localStorage.setItem('email', email)
+    } else {
+      window.localStorage.removeItem('name', name)
+      window.localStorage.removeItem('email', email)
+    }
+
+    submitComment(commentObj)
+      .then((res) => {
+        setShowSuccessMessage(true)
+        setTimeout(() => {
+          setShowSuccessMessage(false);
+        }, 3000)
+      })
 
   }
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
-      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Comment</h3>
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Leave a Reply</h3>
       <div className="grid grid-cols-1 gap-4 mb-4">
         <textarea ref={commentEl} className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" placeholder="Comment" name="comment"/>
 
@@ -31,6 +69,12 @@ const CommentsForm = () => {
 
       </div>
 
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <div>
+          <input ref={storeDataEl} type="checkbox" id="storeData" name="storeData" value="true"/>
+          <label className="text-gray-500 cursor-pointer ml-2">Save my e-mail and name for the next time I comment</label>
+        </div>
+      </div>
       {error && <p className="text-xs text-red-500">All fields are required.</p>}
 
       <div className="mt-8">
