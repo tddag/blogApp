ddfad83cfb7d21d0ac5bb4067bc72a6838fab7a6
Tam Dang <danhtam0509@gmail.com>
2023-05-01 23:02:52 -0400

    finished categories

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,11 +1,16 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 
 import { getPosts, getPostDetails} from '../../services';
 
-import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } from '../../components'
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components'
 
 const PostDetails = ({ post }) => {
+  const router = useRouter();
 
+  if (router.isFallback){
+    return <Loader/>
+  }
   console.log(post)
   return (
     <div className="container mx-auto px-10 mb-8">
