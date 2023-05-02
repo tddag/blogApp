f27d3f3fc4da1e228cf33a17216c9d31343ccd43
Tam Dang <danhtam0509@gmail.com>
2023-05-01 22:33:24 -0400

    finished comments component

--- services/index.js
+++ services/index.js
@@ -130,6 +130,22 @@ export const getCategories = async () => {
     return result.categories    
 }
 
+export const getComments = async (slug) => {
+    const query = gql`
+        query GetComments($slug: String!) {
+            comments(where: { post: { slug: $slug }}) {
+                name
+                createdAt
+                comment
+            }
+        }
+    `
+
+    const result = await request(graphqlAPI, query, { slug })
+
+    return result.comments    
+}
+
 export const submitComment = async (obj) => {
     const result = await fetch('/api/comments', {
         method: 'POST',
